b7996642cc39eae7d0bb1b9b8afb31acca29ead9
MD ARFAKSHAD <149477963+Arfakshad@users.noreply.github.com>
2026-07-31 03:14:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,57 @@
-alert("Welcome to My Portfolio!");
+// Smooth navbar highlight
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 150;
+
+        if (scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
+
+// Fade animation
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.style.opacity="1";
+            entry.target.style.transform="translateY(0)";
+
+        }
+
+    });
+
+});
+
+document.querySelectorAll("section").forEach(section=>{
+
+    section.style.opacity="0";
+    section.style.transform="translateY(40px)";
+    section.style.transition="1s";
+
+    observer.observe(section);
+
+});
